559142be56460d25a271bca76a9c9346bc51ef35
Hielke Morsink <hielke.morsink@gmail.com>
2018-12-25 19:44:34 +0100

    WIP

--- src/openrct2/world/Map.cpp
+++ src/openrct2/world/Map.cpp
@@ -1185,8 +1185,7 @@ void map_reorganise_elements()
 {
     context_setcurrentcursor(CURSOR_ZZZ);
 
-    TileElement* new_tile_elements = (TileElement*)malloc(
-        3 * (MAXIMUM_MAP_SIZE_TECHNICAL * MAXIMUM_MAP_SIZE_TECHNICAL) * sizeof(TileElement));
+    TileElement* new_tile_elements = (TileElement*)malloc(MAX_TILE_ELEMENTS * sizeof(TileElement));
     TileElement* new_elements_pointer = new_tile_elements;
 
     if (new_tile_elements == nullptr)
