e04b66c48aaf6be7307e68e0f72518c69ac81eb0
Ted John <ted@brambles.org>
2019-05-04 12:39:56 +0000

    Add unit tests for command line screenshot

--- test/tests/ImageImporterTests.cpp
+++ test/tests/ImageImporterTests.cpp
@@ -10,8 +10,11 @@
 #include "TestData.h"
 
 #include <gtest/gtest.h>
+#include <openrct2/core/File.h>
 #include <openrct2/core/Path.hpp>
 #include <openrct2/drawing/ImageImporter.h>
+#include <openrct2/interface/Screenshot.h>
+#include <openrct2/world/Climate.h>
 #include <string_view>
 
 using namespace OpenRCT2::Drawing;
@@ -19,6 +22,8 @@ using namespace OpenRCT2::Drawing;
 class ImageImporterTests : public testing::Test
 {
 public:
+    std::string OutputImagePath = "ImageImporterTestsOutput.png";
+
     static std::string GetImagePath(const std::string_view& name)
     {
         return Path::Combine(TestData::GetBasePath(), "images", name.data());
@@ -32,6 +37,11 @@ public:
             hash = (13 * hash) + ((uint8_t*)buffer)[i];
         }
         return hash;
+    }
+
+    void TearDown() override
+    {
+        File::Delete(OutputImagePath);
     }
 };
 
@@ -56,4 +66,39 @@ TEST_F(ImageImporterTests, Import_Logo)
     auto hash = GetHash(result.Buffer, result.BufferLength);
     ASSERT_EQ(0xCEF27C7D, hash);
     free(result.Buffer);
+}
+
+TEST_F(ImageImporterTests, Screenshot)
+{
+    auto parkPath = TestData::GetParkPath("bpb.sv6");
+    auto pngPath = OutputImagePath;
+
+    // Dump screenshot
+    ScreenshotOptions options;
+    const char* argv[] = { parkPath.c_str(), pngPath.c_str(), "800", "600" };
+    auto exitCode = cmdline_for_screenshot(argv, std::size(argv), &options);
+    ASSERT_EQ(1, exitCode);
+
+    // Assert saved image
+    auto image = Imaging::ReadFromFile(pngPath, IMAGE_FORMAT::PNG_32);
+    auto hash = GetHash(image.Pixels.data(), image.Pixels.size());
+    ASSERT_EQ(0xDBA7A237, hash);
+}
+
+TEST_F(ImageImporterTests, GiantScreenshot)
+{
+    auto parkPath = TestData::GetParkPath("bpb.sv6");
+    auto pngPath = OutputImagePath;
+
+    // Dump screenshot
+    ScreenshotOptions options;
+    options.weather = 1 + WEATHER_SUNNY;
+    const char* argv[] = { parkPath.c_str(), pngPath.c_str(), "giant", "0", "0" };
+    auto exitCode = cmdline_for_screenshot(argv, std::size(argv), &options);
+    ASSERT_EQ(1, exitCode);
+
+    // Assert saved image
+    auto image = Imaging::ReadFromFile(pngPath, IMAGE_FORMAT::PNG_32);
+    auto hash = GetHash(image.Pixels.data(), image.Pixels.size());
+    ASSERT_EQ(0xAA740379, hash);
 }
